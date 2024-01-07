f299c49336215ec9ac9abdb750f64f4b4b3728df
alexeylv <101318447+alexeylv@users.noreply.github.com>
2024-01-07 15:10:36 +0000

    Second commit Hello World!
    Second commit Hello World! std::cout << "Hello World!\n" << std::endl;

--- .ccls-cache/@home@runner@ThoughtfulImprobableEfficiency/main.cpp
+++ .ccls-cache/@home@runner@ThoughtfulImprobableEfficiency/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "42" << std::endl;
+  std::cout << "Integrate GitHub" << std::endl;
 }
