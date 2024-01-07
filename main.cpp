79838119561dff1abce8802ecb55ef345d1129b7
alexeylv <101318447+alexeylv@users.noreply.github.com>
2024-01-07 15:22:35 +0000

    Third commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main() {
   std::cout << "Hello World!" << std::endl;
-  std::cout << "42" << std::endl;
+  std::cout << "42/n" << std::endl;
   std::cout << "Integrate GitHub" << std::endl;
 }
