fc23c17a0661853d5809efddc7fcc59b70fbb241
alexeylv <101318447+alexeylv@users.noreply.github.com>
2024-01-07 15:27:48 +0000

    new

--- main.cpp
+++ main.cpp
@@ -3,5 +3,5 @@
 int main() {
   std::cout << "Hello World!" << std::endl;
   std::cout << "42/n" << std::endl;
-  std::cout << "Integrate GitHub" << std::endl;
+  std::cout << "Integrate GitHub/n" << std::endl;
 }
